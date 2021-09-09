08ce261fe6876acb01c5cb67b4e78e9ea3bb920c
York <YorkAARGH@users.noreply.github.com>
2021-09-09 15:18:34 +0100

    Update interactionCreate.js

--- src/listeners/interactionCreate.js
+++ src/listeners/interactionCreate.js
@@ -11,11 +11,19 @@ module.exports = class CommandInteraction extends Listener {
     if (!cmd) return;
     try {
       await cmd.run(interaction);
+      this.container.analytics.increment("misaki.totalCommands");
+      if (process.env.DEV) this.container.logger.info(`${interaction.user.id} ran slash command ${cmd.commandData.name}`);
     } catch (e) {
       this.container.logger.fatal(e);
+
       if (interaction.replied) 
         interaction.followUp({ content: `There was a problem with your request.\n\`\`\`${e.message}\`\`\``, ephemeral: true })
           .catch(e => this.container.logger.fatal("An error occurred following up on an error", e));
+      else 
+
+      if (interaction.deferred)
+        interaction.editReply({ content: `There was a problem with your request.\n\`\`\`${e.message}\`\`\``, ephemeral: true })
+          .catch(e => console.error("An error occurred following up on an error", e));
       else 
         interaction.reply({ content: `There was a problem with your request.\n\`\`\`${e.message}\`\`\``, ephemeral: true })
           .catch(e => this.container.logger.fatal("An error occurred replying on an error", e));
