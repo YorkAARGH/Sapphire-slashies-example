e20846a68c934a6a395201df3c43e6b7f83c9ebd
Jurien Hamaker <whoami@jurien.dev>
2021-09-11 12:47:37 +0200

    feat: update for dev mode (#2)
    * feat: disable global commands on development & add reasoning
    
    * chore: Add another url for reasoning

--- src/lib/structures/SlashCommandStore.js
+++ src/lib/structures/SlashCommandStore.js
@@ -28,6 +28,14 @@ module.exports = class SlashCommandStore extends Store {
       await guild?.commands.set(guildCmds.map(c => c.commandData))
     }
 
+    // Global commands will update over the span of an hour and is discouraged to update on development mode.
+    // https://canary.discord.com/developers/docs/interactions/slash-commands#registering-a-command
+    // https://discord.com/developers/docs/interactions/application-commands#making-a-global-command
+    if(process.env.NODE_ENV === 'development') {
+      this.container.logger.info('Skipped global commands because we\'re in development mode');
+      return;
+    }
+
     // This will register global commands.
     await client?.application?.commands.set(globalCmds.map(c => c.commandData));
   }
