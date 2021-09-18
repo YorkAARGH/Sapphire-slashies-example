cb58cd3f73799bb7f3c64fff186760cba316fe04
UndiedGamer <hemanthchakka2007@gmail.com>
2021-09-18 12:26:51 +0530

    feat: add an example

---
+++ typescript/slashCommands/ping.ts
@@ -0,0 +1,31 @@
+/**
+ * This is the most basic slash command example I can offer you guys, it's not
+ * using the builder but that can easily be changed. If you want your slash
+ * commands to be guild specific, you should consult the discordjs.guide for
+ * how to register them to guilds, here's a link;
+ * https://discordjs.guide/interactions/registering-slash-commands.html#guild-commands
+ *
+ */
+import { PieceContext } from '@sapphire/framework';
+import { SlashCommand } from '../lib/structures/SlashCommandPiece';
+
+module.exports = class Ping extends SlashCommand {
+	constructor(context: PieceContext) {
+		super(context, {
+			name: 'ping',
+			description: 'Pongs when pinged.',
+			options: [],
+			guildOnly: false
+		});
+	}
+
+	async run(interaction) {
+		await interaction.deferReply();
+		const reply = await interaction.editReply('Ping?');
+		await interaction.editReply(
+			`Pong! Latency is ${reply.createdTimestamp - interaction.createdTimestamp}ms. API Latency is ${Math.round(
+				this.container.client.ws.ping
+			)}ms.`
+		);
+	}
+};
