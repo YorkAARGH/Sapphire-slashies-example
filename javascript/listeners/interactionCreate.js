6dcf598482c7c7ce3f7c96ada75b314081464e63
UndiedGamer <hemanthchakka2007@gmail.com>
2021-09-20 16:30:05 +0530

    fix merge conflicts

--- javascript/listeners/interactionCreate.js
+++ javascript/listeners/interactionCreate.js
@@ -35,8 +35,8 @@ module.exports = class CommandInteraction extends Listener {
 
       } else {
         interaction.reply({
-            content: `There was a problem with your request.\n\`\`\`${e.message}\`\`\``,
-            ephemeral: true
+          content: `There was a problem with your request.\n\`\`\`${e.message}\`\`\``,
+          ephemeral: true
         }).catch(e => this.container.logger.fatal("An error occurred replying on an error", e));
       }  
     }
